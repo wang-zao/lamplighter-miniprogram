2871d4ade5ad340e1ec79c8979f14b3b966f429d
王子豪 <wangzihao@megvii.com>
2021-05-19 18:02:50 +0800

    added select panel

--- src/pages/play-minute/play-minute.vue
+++ src/pages/play-minute/play-minute.vue
@@ -1,8 +1,8 @@
 <template>
 <view class="play_minute_wrapper">
   <view class="play_minute_content">
-    <view class="content_panel mile_panel">{{judgeCtrl.totalMiles}}</view>
-    <view class="content_panel info_panel">info_pannel</view>
+    <view class="content_panel mile_panel">{{judgeCtrl.totalMiles.toFixed(0)}}</view>
+    <view class="content_panel info_panel">{{judgeCtrl.restTime}}</view>
     <view class="content_panel ufo_pnnel">
 			<!-- <fly-ufo-showcase /> -->
 			<fly-ufo-responsive
@@ -20,6 +20,12 @@
         v-show="anmtCtrl.crossXVisible"
         @clickedOneDirection="e => handleUserSelected(e)"
       />
+    </view>
+    <view v-if="anmtCtrl.gameEndPageVisible" class="content_panel end_panel">
+      <view class="end_panel_content">
+        <view>Game Over</view>
+        <view>{{judgeCtrl.totalMiles.toFixed(0)}} km</view>
+      </view>
     </view>
   </view>
 </view>
@@ -40,11 +46,13 @@
         anmtCtrl: {
           crossTVisible: false,
           crossXVisible: false,
+          gameEndPageVisible: false,
         },
 				judgeCtrl: {
 					correctDirection: '',
 					distance: '',
           totalMiles: 0,
+          restTime: 60,
 					userSelect: '',
 					isCorrect: false,
           isUserSelected: false,
@@ -62,10 +70,25 @@
         this.cityQueuePopOne();
         this.cityQueuePopOne();
         this.calcAnswer();
+        this.startTimeLoop();
         console.log('init fiinised')
       },
       getCityData() {
         this.cityList = falseCityData;
+      },
+      gameEnd() {
+        this.anmtCtrl.gameEndPageVisible = true;
+      },
+      startTimeLoop() {
+        const clock = setInterval(() => {
+          if (this.judgeCtrl.restTime > 0) {
+            this.judgeCtrl.restTime -= 1;
+          } else if (this.judgeCtrl.restTime <= 0) {
+            clearInterval(clock)
+            console.log('stop!!!')
+            this.gameEnd();
+          }
+        }, 1000)
       },
       calcAnswer() {
         const c = this.currentCity;
@@ -144,6 +167,21 @@ $general-paddng: 1rem;
   .operation_panel {
     bottom: 0;
     justify-content: center;
+  }
+  .end_panel {
+    top: 0;
+    left: 0;
+    background: $dark-mode-mask;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    .end_panel_content {
+      text-align: center;
+      color: #fff;
+    }
   }
 }
 
